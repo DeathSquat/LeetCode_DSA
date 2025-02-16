94f44a08514c14ccf33ca68bcedd3defbba36aeb
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-02-16 22:52:49 +0530

    1718. Construct the Lexicographically Largest Valid Sequence.cpp

---
+++ 57. Construct the Lexicographically Largest Valid Sequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> constructDistancedSequence(int n) {
+        int size = 2 * n - 1;
+        vector<int> result(size, 0);
+        vector<bool> used(n + 1, false);
+        
+        function<bool(int)> backtrack = [&](int index) {
+            if (index == size) return true; // All positions filled successfully
+            
+            if (result[index] != 0) return backtrack(index + 1); // Skip if already filled
+            
+            for (int num = n; num >= 1; --num) { // Try largest number first
+                if (used[num]) continue;
+                
+                if (num == 1) {
+                    result[index] = 1;
+                    used[num] = true;
+                    if (backtrack(index + 1)) return true;
+                    result[index] = 0;
+                    used[num] = false;
+                } else {
+                    int secondIndex = index + num;
+                    if (secondIndex < size && result[secondIndex] == 0) {
+                        result[index] = result[secondIndex] = num;
+                        used[num] = true;
+                        if (backtrack(index + 1)) return true;
+                        result[index] = result[secondIndex] = 0;
+                        used[num] = false;
+                    }
+                }
+            }
+            return false;
+        };
+        
+        backtrack(0);
+        return result;
+    }
+};
