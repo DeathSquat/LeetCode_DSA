71c4770b220b1c7f68666d68e8f11223d5e7bdd4
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-07-17 17:48:41 +0530

    3202. Find the Maximum Length of Valid Subsequence II.cpp

---
+++ 208. Find the Maximum Length of Valid Subsequence II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<unordered_map<int, int>> dp(n);
+        int maxLen = 1;
+
+        for (int i = 0; i < n; ++i) {
+            // Every single element can start a valid sequence of length 1
+            dp[i][0] = 1;
+            for (int j = 0; j < i; ++j) {
+                int mod = (nums[j] + nums[i]) % k;
+                int prevLen = dp[j].count(mod) ? dp[j][mod] : 1;
+                dp[i][mod] = max(dp[i][mod], prevLen + 1);
+                maxLen = max(maxLen, dp[i][mod]);
+            }
+        }
+        return maxLen;
+    }
+};
