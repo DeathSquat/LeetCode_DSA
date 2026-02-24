eb7cb40d11a1385b06ee9bc2032672466578faaa
Trigger <89197100+DeathSquat@users.noreply.github.com>
2026-02-24 20:53:51 +0530

    1022. Sum of Root To Leaf Binary Numbers.cpp

---
+++ 430. Sum of Root To Leaf Binary Numbers.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
+    }
+    
+private:
+    int dfs(TreeNode* node, int current) {
+        if (!node) return 0;
+        
+        // Shift left (multiply by 2) and add current node value
+        current = (current << 1) | node->val;
+        
+        // If leaf node, return the current number
+        if (!node->left && !node->right) {
+            return current;
+        }
+        
+        // Recur for left and right subtrees
+        return dfs(node->left, current) + dfs(node->right, current);
+    }
+};
