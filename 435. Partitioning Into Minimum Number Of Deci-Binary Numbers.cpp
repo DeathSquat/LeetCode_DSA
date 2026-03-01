d9154f75e60e2489c6786bfa1b99de8287e372c7
Trigger <89197100+DeathSquat@users.noreply.github.com>
2026-03-01 21:02:31 +0530

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp

---
+++ 435. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxDigit = 0;
+        
+        for(char c : n) {
+            maxDigit = max(maxDigit, c - '0');
+            
+            // Early stop if we reach 9 (maximum possible digit)
+            if(maxDigit == 9) 
+                return 9;
+        }
+        
+        return maxDigit;
+    }
+};
