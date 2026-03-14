36467005cade6afb157f6becefacb4a684e54bc3
Trigger <89197100+DeathSquat@users.noreply.github.com>
2026-03-14 23:05:17 +0530

    1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp

---
+++ 448. The k-th Lexicographical String of All Happy Strings of Length n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string ans = "";
+    int count = 0;
+
+    void dfs(int n, int k, string &curr) {
+        if (curr.size() == n) {
+            count++;
+            if (count == k) ans = curr;
+            return;
+        }
+
+        for (char c : {'a','b','c'}) {
+            if (!curr.empty() && curr.back() == c) continue;
+            curr.push_back(c);
+            dfs(n, k, curr);
+            curr.pop_back();
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+        dfs(n, k, curr);
+        return ans;
+    }
+};
