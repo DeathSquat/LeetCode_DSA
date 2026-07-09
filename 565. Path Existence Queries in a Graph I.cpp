71a2f91bcaba714ce698ae9c71efb9ee2ef404ba
Trigger <89197100+DeathSquat@users.noreply.github.com>
2026-07-09 13:29:06 +0530

    3532. Path Existence Queries in a Graph I.cpp

--- 565. Path Existence Queries in a Graph I.cpp
+++ 565. Path Existence Queries in a Graph I.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+    vector<bool> pathExistenceQueries(int n,vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         vector<int> component(n, 0);
         int compNo = 0;
         for(int i = 1; i < n; i++){
